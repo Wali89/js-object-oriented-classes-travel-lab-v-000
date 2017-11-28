cc379714150cfc69bacc658fb8dc3e2c623ac77d
Wali Rahman <wali.rahman89@yahoo.com>
2017-11-28 01:21:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ class Driver {
   yearsExperienceFromBeginningOf(dateExp) {
     let diff = (new Date(dateExp) - this.startDate)/1000;
     diff /= (60*60*24);
-    return Math.abs(Math.round(diff/365.25));
+    return Math.abs(Math.round(diff/365));
   }
 }
